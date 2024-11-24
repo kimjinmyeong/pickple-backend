403d2f654c8fa3451f49838d1759dc48362a2697
kimjinmyeong <kjmdkdlel@gmail.com>
2024-11-24 23:48:43 +0900

    refactor: jwtUtil 리팩토링

--- gateway/src/main/java/com/pickple/gateway/infrastructure/security/JwtUtil.java
+++ gateway/src/main/java/com/pickple/gateway/infrastructure/security/JwtUtil.java
@@ -18,33 +18,19 @@ public class JwtUtil {
 
     // Header KEY 값
     public static final String AUTHORIZATION_HEADER = "Authorization";
-    // 사용자 권한 값의 KEY
-    public static final String AUTHORIZATION_KEY = "auth";
+
     // Token 식별자
     public static final String BEARER_PREFIX = "Bearer ";
-    // 토큰 만료시간
-    private final long TOKEN_TIME = 60 * 60 * 1000L; // 60분
 
     @Value("${service.jwt.secret-key}") // Base64 Encode 한 SecretKey
     private String secretKey;
 
     private Key key;
 
-    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-
     @PostConstruct
     public void init() {
         byte[] bytes = Base64.getDecoder().decode(secretKey);
         key = Keys.hmacShaKeyFor(bytes);
-    }
-
-    // JWT 토큰 substring
-    public String substringToken(String tokenValue) {
-        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
-            return tokenValue.substring(BEARER_PREFIX.length());
-        }
-        log.error("Not Found Token");
-        throw new NullPointerException("Not Found Token");
     }
 
     // 토큰 검증
