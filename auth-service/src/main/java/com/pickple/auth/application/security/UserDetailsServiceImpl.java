6dbfc9e3afd4b4cb8eaf3dca281b46cf7f3f9093
kimjinmyeong <kjmdkdlel@gmail.com>
2024-11-24 23:50:40 +0900

    refactor: auth 리팩토링

--- auth-service/src/main/java/com/pickple/auth/application/security/UserDetailsServiceImpl.java
+++ auth-service/src/main/java/com/pickple/auth/application/security/UserDetailsServiceImpl.java
@@ -3,12 +3,15 @@ package com.pickple.auth.application.security;
 import com.pickple.auth.application.domain.model.User;
 import com.pickple.auth.application.dto.UserDto;
 import com.pickple.auth.infrastructure.feign.UserServiceClient;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -18,8 +21,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        UserDto userDto = userServiceClient.getUserByUsername(username);
-        User user = User.convertToUser(userDto);
-        return new UserDetailsImpl(user);
+        try {
+            UserDto userDto = userServiceClient.getUserByUsername(username);
+            User user = User.convertToUser(userDto);
+            return new UserDetailsImpl(user);
+        } catch (FeignException e) {
+            throw new UsernameNotFoundException("사용자가 존재하지 않습니다.", e);
+        }
     }
 }
